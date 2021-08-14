6d4b5adb4b6103711692afd50eab28fc65635464
Guilherme Neves <guilhermes_neves@hotmail.com>
2021-08-14 10:39:41 -0300

    Incluindo método de consultarPet

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -7,16 +7,18 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.contains;
 
 public class Pet {
-    String uri = "https://petstore.swagger.io/v2/pet";
+    String uri = "https://petstore.swagger.io/v2/pet/";
 
     public String lerJson(String caminhoJson) throws IOException {
 
         return new String(Files.readAllBytes(Paths.get(caminhoJson)));
     }
 
-    @Test
+    @Test(priority = 1)
     public void incluirPet() throws IOException {
         String jsonBody = lerJson("db/pet1.json");
 
@@ -29,6 +31,34 @@ public class Pet {
         .then()
                 .log().all()
                 .statusCode(200)
+                .body("name", is("Thor"))
+                .body("status", is("available"))
+                .body("category.id", is(1))
+                .body("category.name", is("dog"))
         ;
+    }
+
+    @Test(priority = 2)
+    public void consultarPet() {
+        String petId = "1996040925";
+
+
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .get(uri + petId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Thor"))
+                .body("status", is("available"))
+                .body("category.id", is(1))
+                .body("category.name", is("dog"))
+        //.extract()
+          //      .path("category.name")
+        ;
+
+        //System.out.println(token);
     }
 }
